4b391b11d1d6fd91b8ff6a4d867d81ee04a71c63
TeamDrag <43828218+TeamDrag@users.noreply.github.com>
2018-10-05 04:07:28 -0700

    Create Splash.java

---
+++ Splash.java
@@ -0,0 +1,24 @@
+package com.example.vasu.projectdrag;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+    static int splashtime=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashtime);
+    }
+}
