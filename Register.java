4ba615189ed22f2dcb4c762b308b5488b4cd4c88
TeamDrag <43828218+TeamDrag@users.noreply.github.com>
2018-10-06 00:43:04 +0530

    Update Register.java

--- Register.java
+++ Register.java
@@ -1,1 +1,49 @@
-hjbvfbve
+package com.example.shivamdhammi.drag;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+
+    Button sso,donor,already;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        sso = (Button) findViewById(R.id.id_sso);
+        donor = (Button) findViewById(R.id.id_donor);
+        already = (Button) findViewById(R.id.id_already);
+
+        sso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SSOreg.class);
+                startActivity(intent);
+            }
+        });
+
+        donor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),DonorReg.class);
+                startActivity(intent);
+            }
+        });
+
+        already.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
+
