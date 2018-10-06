f9883ecf811ace1aef4cabc3f376d4ff46ad39eb
TeamDrag <43828218+TeamDrag@users.noreply.github.com>
2018-10-07 00:27:38 +0530

    Update SSOReg.java

--- SSOReg.java
+++ SSOReg.java
@@ -69,7 +69,7 @@ public class SSOReg extends AppCompatActivity {
 
             Toast.makeText(getApplicationContext(),"Registered Successfully..",Toast.LENGTH_LONG).show();
 
-            Intent intent = new Intent(getApplicationContext(),Login.class);
+            Intent intent = new Intent(getApplicationContext(),Home.class);
             startActivity(intent);
 
         }
