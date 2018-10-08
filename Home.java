8eabbbd19bc87f287e6e5b0ddc706644f295212a
TeamDrag <43828218+TeamDrag@users.noreply.github.com>
2018-10-08 21:52:17 +0530

    Add files via upload

--- Home.java
+++ Home.java
@@ -1,37 +1,32 @@
-package com.example.vasu.projectdrag;
+package com.example.mayank.sso;
 
-import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
-    Button register,login;
-
+    Button b;
+    private ConstraintLayout constraintLayout;
+    private AnimationDrawable animationDrawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        register=(Button)findViewById(R.id.id_register);
-        login=(Button)findViewById(R.id.id_login);
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
 
-        register.setOnClickListener(new View.OnClickListener() {
+        b=(Button)findViewById(R.id.id_login);
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent =new Intent(getApplicationContext(),Register.class);
-                startActivity(intent);
-            }
-        });
-
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),Login.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                b.setShadowLayer(2,5,5,156548);
             }
         });
     }
