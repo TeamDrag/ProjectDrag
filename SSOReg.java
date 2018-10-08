fcb7b06ff691f12d62675e5a95a3eb19cbc5ddc7
TeamDrag <43828218+TeamDrag@users.noreply.github.com>
2018-10-08 17:23:32 +0530

    Update SSOReg.java

--- SSOReg.java
+++ SSOReg.java
@@ -1,163 +1,38 @@
 package com.example.shivamdhammi.drag;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+public class SSOInfo {
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+    private String SSOName,ISOnumber,Email,Address,Contact;
 
-import org.w3c.dom.Text;
-
-public class SSOReg extends AppCompatActivity {
-
-    private EditText UserName,Password,RePassword,SSOName,ISOnumber,Email,Address,Contact;
-    private Button Register;
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
-    private FirebaseAuth mAuth;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ssoreg);
-
-        UserName = (EditText)findViewById(R.id.id_username);
-        Password = (EditText)findViewById(R.id.id_password);
-        RePassword = (EditText)findViewById(R.id.id_repassword);
-        SSOName = (EditText)findViewById(R.id.id_ssoname);
-        ISOnumber = (EditText)findViewById(R.id.id_isonumber);
-        Email = (EditText)findViewById(R.id.id_email);
-        Address = (EditText)findViewById(R.id.id_address);
-        Contact = (EditText)findViewById(R.id.id_contact);
-        Register = (Button)findViewById(R.id.id_register);
-
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("SSO");
-
-        mAuth = FirebaseAuth.getInstance();
-
-        Register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addUser();
-            }
-        });
-
+    SSOInfo(){
 
     }
 
-    private void addUser(){
-
-        final String username = UserName.getText().toString().trim();
-        String password = Password.getText().toString().trim();
-        String repassword = RePassword.getText().toString().trim();
-        String ssoname = SSOName.getText().toString().trim();
-        String isonumber = ISOnumber.getText().toString().trim();
-        String email = Email.getText().toString().trim();
-        String address = Address.getText().toString().trim();
-        String contact = Contact.getText().toString().trim();
-
-        if(!TextUtils.isEmpty(username)){
-
-            if(password.equals(repassword)) {
-
-                if(Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-
-                    if(!contact.isEmpty()&&(contact.length()==10)) {
-
-                        if(!ssoname.isEmpty()) {
-
-                            if(!isonumber.isEmpty()) {
-
-                                final SSOInfo info = new SSOInfo(username,ssoname, isonumber, email, address, contact);
-
-
-                                //Log.d("shivam","Dhammi");
-
-                                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        if(task.isSuccessful()){
-                                            myRef.child(username).setValue(info);
-                                            
-                                            Toast.makeText(getApplicationContext(), "Registered Successfully..", Toast.LENGTH_LONG).show();
-                                            
-                                            Intent intent = new Intent(getApplicationContext(), Login.class);
-                                                startActivity(intent);
-
-                                        }
-                                        else{
-
-                                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
-                                                Toast.makeText(getApplicationContext(),"Email is already registered",Toast.LENGTH_LONG).show();
-                                            }
-                                            else if(task.getException() instanceof FirebaseAuthWeakPasswordException){
-                                                Toast.makeText(getApplicationContext(),"Password is too weak",Toast.LENGTH_LONG).show();
-                                            }
-                                            else{
-                                                Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
-                                            }
-                                        }
-                                    }
-                                });
-
-                            }
-                            else{
-                                ISOnumber.setError("Please enter ISO number");
-                                ISOnumber.requestFocus();
-                                return;
-                            }
-                        }
-                        else{
-                            SSOName.setError("Please Enter the SSO name.");
-                            SSOName.requestFocus();
-                            return;
-                        }
-                    }
-                    else{
-                        Contact.setError("Please enter the mobile number");
-                        Contact.requestFocus();
-                        return;
-                    }
-                }
-
-                else{
-                    Email.setError("Enter a valid Email");
-                    Email.requestFocus();
-                    return;
-                }
-            }
-            else{
-                RePassword.setError("Password didn't match. Try again.");
-                RePassword.requestFocus();
-                return;
-            }
-
-        }
-        else{
-            UserName.setError("Please Enter The UserName..");
-            UserName.requestFocus();
-            return;
-        }
-
-
+    public SSOInfo(String SSOName, String ISOnumber, String email, String address, String contact) {
+        this.SSOName = SSOName;
+        this.ISOnumber = ISOnumber;
+        Email = email;
+        Address = address;
+        Contact = contact;
     }
 
+    public String getSSOName() {
+        return SSOName;
+    }
 
+    public String getISOnumber() {
+        return ISOnumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public String getContact() {
+        return Contact;
+    }
 }
