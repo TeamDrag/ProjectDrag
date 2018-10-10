c575ef79341ac2f9a8ca19c9a138d3b3c19fc81d
shivamdhammi <43827900+shivamdhammi@users.noreply.github.com>
2018-10-10 15:39:51 +0530

    Update SSOInfo.java

--- SSOInfo.java
+++ SSOInfo.java
@@ -1,17 +1,24 @@
 package com.example.shivamdhammi.drag;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SSOInfo {
 
     private String UserName,SSOName,ISOnumber,Email,Address,Contact;
 
-    SSOInfo(){
+    public Map<String, Boolean> stars = new HashMap<>();
+
+    public SSOInfo(){
 
     }
 
-    public SSOInfo(String UserName,String SSOName, String ISOnumber, String email, String address, String contact) {
-        this.UserName = UserName;
-        this.SSOName = SSOName;
-        this.ISOnumber = ISOnumber;
+    public SSOInfo(String userName,String ssoname, String isonumber, String email, String address, String contact) {
+        UserName = userName;
+        SSOName = ssoname;
+        ISOnumber = isonumber;
         Email = email;
         Address = address;
         Contact = contact;
@@ -40,4 +47,46 @@ public class SSOInfo {
     public String getContact() {
         return Contact;
     }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public void setSSOName(String ssoname) {
+        this.SSOName = ssoname;
+    }
+
+    public void setISOnumber(String isOnumber) {
+        this.ISOnumber = isOnumber;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public void setContact(String contact) {
+        Contact = contact;
+    }
+
+    
+    //ye ignore kr bhai
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("username", UserName);
+        result.put("ssoname", SSOName);
+        result.put("isonumber", ISOnumber);
+        result.put("contact", Contact);
+        result.put("address", Address);
+        result.put("email", Email);
+
+        return result;
+    }
+
+
+
 }
