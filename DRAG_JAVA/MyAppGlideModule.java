7a976b09128c5524dc4d8b8b80385193a108002d
shivamdhammi <43827900+shivamdhammi@users.noreply.github.com>
2018-10-11 23:05:11 +0530

    Create MyAppGlideModule.java

---
+++ DRAG_JAVA/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+//bas ye likh dio, aur kuch nhi.
+//same folder me rkhio, jisme baki java files hai.
+package com.example.shivamdhammi.drag;
+
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+}
